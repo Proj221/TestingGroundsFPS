d845f29929d67fe65f47ce46ae1f13db6a7cd449
Huawei <ravidren@126.com>
2021-05-21 21:42:09 +0800

    Ep203_LFS Enabled & Added FPS content

---
+++ TestingGroundFPS/Source/TestingGroundFPS.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TestingGroundFPSTarget : TargetRules
+{
+	public TestingGroundFPSTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("TestingGroundFPS");
+	}
+}
