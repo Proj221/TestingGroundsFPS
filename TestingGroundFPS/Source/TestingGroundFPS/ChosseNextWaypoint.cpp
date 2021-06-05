2eb18af4b453100c4e46ffa19e83e5873b18149d
HuaweiREN <ravidren@126.com>
2021-06-05 15:03:37 +0800

    Ep217_Using CPP to expose variables to BT

--- TestingGroundFPS/Source/TestingGroundFPS/ChosseNextWaypoint.cpp
+++ TestingGroundFPS/Source/TestingGroundFPS/ChosseNextWaypoint.cpp
@@ -2,8 +2,12 @@
 
 
 #include "ChosseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChosseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	UE_LOG(LogTemp, Warning, TEXT("NextWaypoint in CPP"));
+	
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("NextWaypoint idx is: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
--- TestingGroundFPS/Source/TestingGroundFPS/ChosseNextWaypoint.h
+++ TestingGroundFPS/Source/TestingGroundFPS/ChosseNextWaypoint.h
@@ -17,5 +17,7 @@ class TESTINGGROUNDFPS_API UChosseNextWaypoint : public UBTTaskNode
 public:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
-	
+protected:
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+	struct FBlackboardKeySelector IndexKey;
 };
